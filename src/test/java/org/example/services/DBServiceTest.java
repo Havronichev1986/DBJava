1a45f7e968730da46e55289145b55c7a8e6cec8d
IDK7234 <clewe2008@mail.ru>
2024-11-21 21:03:05 +0300

    create Test

--- src/test/java/org/example/services/DBServiceTest.java
+++ src/test/java/org/example/services/DBServiceTest.java
@@ -1,19 +1,25 @@
 package org.example.services;
 
-
 import lombok.extern.slf4j.Slf4j;
 import org.example.entities.PersonEntity;
 import org.example.repositories.PersonRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.List;
+import java.util.stream.Stream;
+
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatNullPointerException;
+import static org.assertj.core.api.InstanceOfAssertFactories.STREAM;
+import static org.mockito.Mockito.lenient;
 import static org.mockito.Mockito.when;
 
 @Slf4j
@@ -25,46 +31,43 @@ public class DBServiceTest {
     @Mock
     PersonRepository personRepository;
 
-    @Test
-    @DisplayName("Тест getPersons")
+    private static final List<Long> ids = List.of(1L,2L);
+    private static final List<PersonEntity> personEntities1 = List.of(new PersonEntity(1L,"Василий",(short)20));
+
+    @BeforeEach
+    void setUp(){
+        lenient().when(personRepository.findAllById(ids)).thenReturn(personEntities1);
+        lenient().when(personRepository.findAll()).thenReturn(personEntities1);
+        lenient().when(personRepository.saveAll(personEntities1)).thenReturn(personEntities1);
+    }
+
+    public static Stream<List<Long>> parameters(){
+        return Stream.of(ids,null);
+    }
+
+
+    @DisplayName("Получние персон")
+    @ParameterizedTest(name = "{index} - {0} is a palindrome")
+    @MethodSource("parameters")
     //Подготовка тестов данных
-    void getPersons(){
-        var ids = List.of(1L,2L);
-        var personEntities1 = List.of(new PersonEntity(1L,"Василий",(short)20));
-        var personEntities2 = List.of(new PersonEntity(2L,"Петр",(short)35));
-        when(personRepository.findAllById(ids)).thenReturn(personEntities1);
-        when(personRepository.findAll()).thenReturn(personEntities2);
+    void getPersons(List<Long>ids){
         //Тест
         List<PersonEntity> personEntitiesA = dbService.getPersons(ids);
-        List<PersonEntity> personEntitiesB = dbService.getPersons(null);
         log.info("A: {}" , personEntitiesA);
-        log.info("B: {}" , personEntitiesB);
         log.info("1: {}" , personEntities1);
-        log.info("2: {}" , personEntities2);
         //проверка результатов
         assertThat(personEntitiesA).isEqualTo(personEntities1);
-        assertThat(personEntitiesB).isEqualTo(personEntities2);
     }
 
     @Test
-    @DisplayName("Тест createPersons")
+    @DisplayName("Создание персон")
     void createPersons(){
-        var personEntities1 = List.of(new PersonEntity(1L,"Василий",(short)20));
-        // Подготовка мока для метода save
-        when(personRepository.saveAll(personEntities1)).thenReturn(personEntities1);
-        //Test
-        List<PersonEntity> createdPersons = dbService.createPersons(personEntities1);
-
-        //Проверка результата
-        assertThat(createdPersons).isEqualTo(personEntities1);
-
+        List<PersonEntity> personEntities = dbService.createPersons(personEntities1);
+        assertThat(personEntities).isEqualTo(personEntities1);
     }
     @Test
+    @DisplayName("Удаление персон")
     void deletePersons(){
-        var ids = List.of(1L,2L);
-        var personEntities1 = List.of(new PersonEntity(1L,"Василий",(short)20));
-        var personEntities2 = List.of(new PersonEntity(2L,"Петр",(short)35));
-        when(personRepository.findAllById(ids)).thenReturn(personEntities1);
         List<PersonEntity> personEntities = dbService.deletePersons(ids);
         assertThat(personEntities).isEqualTo(personEntities1);
     }
